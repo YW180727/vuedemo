1fbae187998dab8e1c92806860097e9fab262162
Ygw960621 <15035419731@163.com>
2019-09-27 11:03:20 +0800

    add header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,6 +1,22 @@
 <template>
-<div>
-    hello word this is header!
+<div class="header">
+  <div class="header-left">
+      <div class="iconfont back-icon">
+          &#xe624;
+      </div>
+
+   </div>
+  <div class="header-input">
+      <span class="iconfont">
+          &#xe632;
+      </span>
+
+      输入城市/景点/游玩主题</div>
+  <div class="header-right">城市
+        <span class="iconfont arrow-iconf">
+          &#xe64a;
+      </span>
+  </div>
 </div>
 </template>
 <script>
@@ -8,7 +24,34 @@ export default {
   name: 'HomeHeader'
 }
 </script>
-<style >
- 
-
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .header
+        display: flex;
+        line-height:.86rem;
+        background :$bgColor;
+        color :#fff;
+        .header-left
+            width:.64rem;
+            float: left;
+            .back-icon
+                text-align :center;
+                font-size:.4rem;
+        .header-input
+            flex:1;
+            line-height :.64rem;
+            height :.64rem;
+            background :#fff;
+            border-radius:.1rem;
+            margin-top :.12rem;
+            margin-left :.2rem;
+            color :#ccc;
+            padding-left :.2rem;
+        .header-right
+            width:1.24rem;
+            float:right; 
+            text-align :center;
+            .arrow-iconf
+                margin-left :-.04rem;
+                font-size:.24rem;
 </style>
