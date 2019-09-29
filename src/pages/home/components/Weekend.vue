bb85a73e1b707f0b4b0403c574eebc7d6faabdbe
Ygw960621 <15035419731@163.com>
2019-09-29 09:34:22 +0800

    weeked

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <div class="recommend-title">周末去哪儿</div>
+        <div class="title">周末去哪儿</div>
         <ul>
             <li class="item" border-bottom v-for="item in recommendList" :key="item.id">
-                <div class="item-img-wrapper"></div>
                     <div class="item-img-wrapper">
                         <img class="item-img" :src="item.imgUrl"/>
                     </div>
@@ -11,7 +10,6 @@
                      <div class="item-info">
                         <p class="item-title">{{item.title}}</p>
                         <p class="item-desc">{{item.desc}}</p>
-                        <button class="item-button">查看详情</button>    
                     </div>  
                 
 
@@ -61,38 +59,28 @@ export default {
 </script>
 <style lang="stylus" scoped>
 @import '~styles/mixins.styl'
-    .recommend-title
+    .title
         margin-top :.2rem;
         line-height :.8rem;
         background :#eee;
         text-indent :.2rem
     .item-img-wrapper
+        height :0;
+        overflow hidden;
+        padding-bottom:33.9%
 
-    .item-img
-            width :1.7rem;
-            height:1.7rem;
-            padding : .1rem;
+        .item-img
+                width :100%;
     .item-info
-        flex: 1 
-        padding .1rem;
-        min-width 0
-        .item-title
-            line-height .54rem
-            font-size .32rem
-            ellipsis()
+        padding .1rem
+    .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
 
-        .item-desc   
-            line-height .4rem
-            color #ccc
-            ellipsis()
-
-        .item-button
-            background #ff9300
-            padding 0 .2rem
-            border-redius .06rem
-            margin-top .2rem
-            color #fff
-            line-height .44rem
-            margin-top .16rem
+    .item-desc   
+        line-height .4rem
+        color #ccc
+        ellipsis()
 
 </style>
